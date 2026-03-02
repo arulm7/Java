4ccfb8c5f05146c95e8bb7be680d55c519f72d55
Arul <arul072004@gmail.com>
2026-03-03 05:05:00 +0530

    -

---
+++ Add.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Add
+{
+    public static void main(String[] args){
+        int a,b,sum;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of a : ");
+        System.out.println("Enter the value of b : ");
+        a = sc.nextInt();
+        b=sc.nextInt();
+        sum = a+b;
+        System.out.println("The sum of "+a+" and "+b+" is : " + sum);
+    }
+}
