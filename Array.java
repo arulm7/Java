b4f647b14b818ca50481360ebb11a7f614256a1d
Arul <arul072004@gmail.com>
2026-03-08 21:41:05 +0530

    -

---
+++ Array.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Array {
+    public static void main(String args[]){
+        int a[] = new int[10],i,n;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of elements in the array : ");
+        n =sc.nextInt();
+        for(i=0;i<n;i++){
+            System.out.println("Enter the element "+(i+1)+" : ");
+            a[i] = sc.nextInt();
+        }
+        for(i=0;i<n;i++){
+            System.out.print(a[i]+" ");
+        }
+
+    }
+    
+}
