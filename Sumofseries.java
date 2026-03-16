3cdbb500c26567d0d30a666c901b5bde2019e8e6
Arul <arul072004@gmail.com>
2026-03-16 21:18:22 +0530

    -

---
+++ Sumofseries.java
@@ -0,0 +1,16 @@
+//sum of series
+import java.util.*;
+
+class Sumofseries{
+    public static void main(String[] args){
+        int n;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the n terms : ");
+        n = sc.nextInt();
+        int sum = 0;
+        for(int i=1; i<=n; i++){
+            sum = sum + (i*i);
+        }
+        System.out.println("Sum of the series is : " + sum);
+    }
+}
