76ffc54bd3b81cd5bf5975c69f5e708497f61bc8
Arul <arul072004@gmail.com>
2026-03-22 15:40:48 +0530

    tcs2

---
+++ Ballooncapacity.java
@@ -0,0 +1,30 @@
+import java.util.Scanner; 
+public class Ballooncapacity {
+    public static void main(String[] args){
+        //input is like n persons and n[i] is weight of persons and x is capacity of balloon and it should give maximum members can enter the baloon
+        int n,x,i;
+        int[] w = new int[100];
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of persons : ");
+        n = sc.nextInt();
+        System.out.println("Enter the weights of persons : ");
+        for(i=0;i<n;i++){
+            w[i] = sc.nextInt();
+        }
+        System.out.println("Enter the capacity of balloon : ");
+        x = sc.nextInt();
+        sc.close();
+        int count = 0;
+        int sum = 0;
+        for(i=0;i<n;i++){
+            if(sum + w[i] <= x){
+                sum += w[i];
+                count++;
+            } else {
+                break;
+            }
+        }
+        System.out.println("Maximum members that can enter the balloon: " + count);
+
+    }
+}
