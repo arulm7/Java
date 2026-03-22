d7371b96998e974be6ddd67ac4a47d729ccbf265
Arul <arul072004@gmail.com>
2026-03-22 15:42:18 +0530

    tcs2 updated

--- Ballooncapacity.java
+++ Ballooncapacity.java
@@ -1,7 +1,7 @@
 import java.util.Scanner; 
 public class Ballooncapacity {
     public static void main(String[] args){
-        //input is like n persons and n[i] is weight of persons and x is capacity of balloon and it should give maximum members can enter the baloon
+        //input is like n persons and n[i] is weight of persons we need to assending order of weights and x is capacity of balloon and it should give maximum members can enter the baloon
         int n,x,i;
         int[] w = new int[100];
         Scanner sc = new Scanner(System.in);
@@ -16,6 +16,17 @@ public class Ballooncapacity {
         sc.close();
         int count = 0;
         int sum = 0;
+        // Sort the weights in ascending order
+        for(i=0;i<n-1;i++){
+            for(int j=0;j<n-1-i;j++){
+                if(w[j] > w[j+1]){
+                    int temp = w[j];
+                    w[j] = w[j+1];
+                    w[j+1] = temp;
+                }
+            }
+        }
+        // Calculate the maximum members that can enter the balloon
         for(i=0;i<n;i++){
             if(sum + w[i] <= x){
                 sum += w[i];
